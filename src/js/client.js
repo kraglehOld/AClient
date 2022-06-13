7ba7de415712b658e619f5b8a1ccd18898710988
kragleh <88108564+kragleh@users.noreply.github.com>
2022-06-13 17:53:57 +0200

    Update client.js

--- src/js/client.js
+++ src/js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://node.rizn.top:25503');
+const socket = io('https://node.rizn.top:25503');
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
